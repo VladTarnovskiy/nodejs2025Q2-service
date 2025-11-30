8f7c7f46c27e9bb4650a01ad0038b6e8ba696ecc
Vladislav Tarnovskiy <v.arnovskiy@georank.ru>
2025-11-30 23:31:17 +0300

    ref: import favorite module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,17 @@ import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
+import { FavoritesModule } from './favorite/favorite.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, ArtistModule, AlbumModule, TrackModule],
+  imports: [
+    DatabaseModule,
+    UserModule,
+    ArtistModule,
+    AlbumModule,
+    TrackModule,
+    FavoritesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
